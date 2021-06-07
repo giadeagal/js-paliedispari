6112dd39e94c248cb4777117a1343ae87ca875fb
Gianluca <gl.galassini@gmail.com>
2021-06-07 18:29:21 +0200

    soluzione alternativa

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 var parola = prompt("Inserisci una parola");
 
-function palindromo(x) {
-    for (i = 0; i < x.length / 2; i++) {
-        console.log(x[i])
-    };
-    for (i = x.length; i >= i.length / 2 ; i--) {
-        console.log(x[i])
-    };
-};
 
-palindromo(parola);
+// function palindromo(x) {
+//     var controllo = false;
+//     for (var i = 0; i < x.length / 2; i++) {
+//         if (x[i] == x[x.length - 1 - i]) {
+//             controllo = true;
+//         } else {
+//             controllo = false
+//         }
+//     } return controllo;
+// }
+
+// var risultato = palindromo(parola)
+// console.log(risultato)
+
+function palindromo(x) {
+    var reverse = "";
+    for (var i = x.length - 1; i >= 0; i--) {
+        reverse += x[i];
+    }
+    return reverse;
+}
+
+var risultato = palindromo(parola);
+
+console.log(risultato);
