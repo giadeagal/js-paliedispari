5240dd5f09049678dfd8ee4ff2ec2496a02fe0dc
Gianluca <gl.galassini@gmail.com>
2021-06-07 15:48:51 +0200

    cose che non funzionano

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-function init () {
-    document.getElementById("printer").innerHTML = "Hello World!"
-};
+var parola = prompt("Inserisci una parola");
+var palindromo = false;
 
-init ();
+function palin(parola) {
+    for (i = 0; i<parola.length/2; i++) {
+        if (parola.charAt(i) == parola.charAt(i + parola.length / 2 -1)) {
+            return true;
+        }
+    }
+}
+console.log(palindromo)
