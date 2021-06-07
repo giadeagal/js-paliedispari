65e9138ea34f43fa62720d4b3a74c6cbdf7d351c
Gianluca <gl.galassini@gmail.com>
2021-06-07 16:50:18 +0200

    tolgo il doppio array

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 var parola = prompt("Inserisci una parola");
-var firstHalf = [];
-var secondHalf = [];
 
 function palindromo(x) {
     for (i = 0; i < x.length / 2; i++) {
-        firstHalf.push(x.charAt(i))
+        console.log(x[i])
     };
-    for (i = x.length; i >= i.length / 2 + 1; i--) {
-        secondHalf.push(x.charAt(i));
+    for (i = x.length; i >= i.length / 2 ; i--) {
+        console.log(x[i])
     };
 };
 
 palindromo(parola);
-
-console.log(firstHalf + secondHalf);
